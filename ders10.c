2da31ff8c0646f4ace161d2805b16a9dd023e40f
tansu dogan <tansudogan3861@gmail.com>
2023-06-13 08:06:16 +0300

    Eklendi

---
+++ ders10.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	/*
+	int i;
+	
+	for(i=1;i<10;i++)
+	{
+		printf("Gunaydin Trabzon\n");
+	}
+	*/
+	
+	/*
+	int j;
+	for(j=0;j<=10;j++)
+	{
+		printf("%d. Gunaydin Kayseri\n",j+1);
+	}
+	
+	*/
+	
+	/*
+	// 0'dan 20'ye kadar olan çift sayilari ekrana yazdýrma
+	int a;
+	
+	for(a=0;a<=20;a+=2)
+	{
+		printf("Sayi: %d\n",a);
+	}
+	*/
+	
+	int i;
+	for(i=0;i<=30;i+=4)
+	{
+		printf("%d. Ýyi Calismalar Ankara\n",i);
+	}
+	
+	return 0;
+}
